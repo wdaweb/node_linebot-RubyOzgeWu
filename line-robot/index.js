a261e65ab9efa23079f717de44867701be0dfac4
Ruby Oz <rubyozgewu@gmail.com>
2022-05-23 14:26:44 +0800

    第一次上傳

---
+++ line-robot/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import linebot from 'linebot'
+import axios from 'axios'
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動')
+})
